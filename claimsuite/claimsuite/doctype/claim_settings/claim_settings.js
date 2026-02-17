5fc13fd00ae35769a9a4dad2e1c041adbda4941e
fadilsiddique <fadilsiddique@gmail.com>
2026-02-17 19:25:41 +0000

    doctypes for claim suite

---
+++ claimsuite/claimsuite/doctype/claim_settings/claim_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Upscape and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Claim Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ claimsuite/claimsuite/doctype/claim_settings/claim_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Upscape and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ClaimSettings(Document):
+	pass
